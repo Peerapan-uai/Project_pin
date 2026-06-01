90013d7bfb006c2157078cea65bcb88c84ac0814
Peerapan-uai <peerapan.uai@gmail.com>
2026-06-02 04:00:20 +0700

    fix(db): add SSL support for Aiven MySQL connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const mysql = require('mysql2/promise')
+require('dotenv').config()
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -7,10 +7,11 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
   timezone: '+00:00',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-});
+})
 
-module.exports = pool;
+module.exports = pool
